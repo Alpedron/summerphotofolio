e36ba6c97fb9d1f7a331cabeee7124e7e5bbd6ed
Adrish P <87411245+AdrishLP@users.noreply.github.com>
2025-04-10 06:45:27 -0400

    recommit weather

--- js/script.js
+++ js/script.js
@@ -111,7 +111,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const greetingText = matched?.newEntry
       ? `${greeting} ${storedName}! Welcome to Natsu's site. It's ${time} EST on ${date}, and currently the weather in Detroit is: ${cachedWeather}.`
-      : `${greeting} ${storedName}! Glad to see you back. It's ${time} EST on ${date}, and currently the weather is: ${cachedWeather}.`;
+      : `${greeting} ${storedName}! Glad to see you back. It's ${time} EST on ${date}, and currently the weather in Detroit is: ${cachedWeather}.`;
 
     const lastVisitText = matched ? `You last visited on ${matched.lastVisit}.` : "";
 
